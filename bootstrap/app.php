d99103172b565826ded7617d9255e386070fef76
Saifutdinov Oleg <zeusfly@mail.ru>
2025-11-17 02:17:00 +0300

    Modified on server

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \App\Http\Middleware\CorsMiddleware::class,
         ]);
-        $middleware->statefulApi();
+        // $middleware->statefulApi(); // Используем токен-аутентификацию вместо stateful API
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
