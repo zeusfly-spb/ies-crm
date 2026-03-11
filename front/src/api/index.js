b54ec5d7b4141acc85f388b028dc178763d0c448
Олег Сайфутдинов <zeusfly@mail.ru>
2026-03-11 12:19:51 +0300

    Fixed front port

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import router from '../router';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
